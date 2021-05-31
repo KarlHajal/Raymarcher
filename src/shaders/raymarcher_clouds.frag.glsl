96be2515772d2a4a46d1aff025737d30135ce10a
karlhajal <karlhajal@gmail.com>
2021-05-31 12:42:55 +0200

    simplified clouds

--- src/shaders/raymarcher_clouds.frag.glsl
+++ src/shaders/raymarcher_clouds.frag.glsl
@@ -16,14 +16,18 @@ uniform float current_time;
 
 float time = current_time/20.;
 
-const vec3 sunDir = vec3(-1,.2,-1);
+const vec3 sun_direction = vec3(-1, .2, -1);
+
+vec3 to_gamma_color( vec3 col )
+{
+	return pow( col, vec3(1.0/GAMMA) );
+}
 
 vec4 mod289(vec4 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
 
 vec4 perm(vec4 x) { return mod289(((x * 34.0) + 1.0) * x); }
 
-float noise(vec3 p)
-{
+float noise(vec3 p){
 	vec3 a = floor(p);
 	vec3 d = p - a;
 	d = d * d * (3.0 - 2.0 * d);
@@ -40,33 +44,40 @@ float noise(vec3 p)
 	return o4.y * d.y + o4.x * (1.0 - d.y);
 }
 
-vec4 map( vec3 p )
-{
+vec4 map( vec3 p ){
 	float den = -1.0 - (abs(p.y-0.5)+0.5)/2.0;
 
     // clouds
 	float f;
 	vec3 q = p*.5 - vec3(0.0,0.0,1.5)*time + vec3(sin(0.7*time),0,0);
-    f  = 0.50000*noise( q ); q = q*2.02 - vec3(0.0,0.0,0.0)*time;
-    f += 0.25000*noise( q ); q = q*2.03 - vec3(0.0,0.0,0.0)*time;
-    f += 0.12500*noise( q ); q = q*2.01 - vec3(0.0,0.0,0.0)*time;
-    f += 0.06250*noise( q ); q = q*2.02 - vec3(0.0,0.0,0.0)*time;
-    f += 0.03125*noise( q );
+	
+    f  = 0.50000 * noise(q); 
+	q *= 2.02;
 
-	den = clamp( den + 4.0*f, 0.0, 1.0 );
+    f += 0.25000 * noise(q); 
+	q *= 2.03;
+
+    f += 0.12500 * noise(q);
+	q *= 2.01;
+
+    f += 0.06250 * noise(q); 
+	q *= 2.02;
+
+    f += 0.03125 * noise(q);
+
+	den = clamp(den + 4.0*f, 0.0, 1.0);
 	
-	vec3 col = mix( vec3(1.0, 1.0, 1.0), vec3(0.6,0.5,0.4), den*.5 );// + 0.05*sin(p);
+	vec3 col = mix(vec3(1.0, 1.0, 1.0), vec3(0.6,0.5,0.4), den*.5 );
 	
-	return vec4( col, den*.7 );
+	return vec4(col, den*.7);
 }
 
-float testshadow( vec3 p, float dither )
-{
+float testshadow( vec3 p, float dither ){
 	float shadow = 1.0;
 	float s = 0.0; // this causes a problem in chrome: .05*dither;
 	for ( int j=0; j < 5; j++ )
 	{
-		vec3 shadpos = p + s*sunDir;
+		vec3 shadpos = p + s*sun_direction;
 		shadow = shadow - map(shadpos).a*shadow;
 		
 		s += .05;
@@ -74,8 +85,7 @@ float testshadow( vec3 p, float dither )
 	return shadow;
 }
 
-vec3 raymarch( vec3 ro, vec3 rd )
-{
+vec3 raymarch( vec3 ro, vec3 rd ){
 	vec4 sum = vec4( 0 );
 	
 	float t = 0.0;
@@ -108,19 +118,11 @@ vec3 raymarch( vec3 ro, vec3 rd )
 	return clamp( sum.xyz, 0.0, 1.0 );
 }
 
-vec3 to_gamma_color( vec3 col )
-{
-	// convert back into colour values, so the correct light will come out of the monitor
-	return pow( col, vec3(1.0/GAMMA) );
-}
-
 void main() {
 	vec3 ray_origin = v2f_ray_origin;
 	vec3 ray_direction = normalize(v2f_ray_direction);
 	
-    // raymarch	
-	vec3 col = raymarch( ray_origin, ray_direction );
+	vec3 pix_color = raymarch( ray_origin, ray_direction );
 
-	col = to_gamma_color(col);
-	gl_FragColor = vec4(col, 1.);
+	gl_FragColor = vec4(to_gamma_color(pix_color), 1.);
 }
